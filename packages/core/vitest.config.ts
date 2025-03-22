fcb51ce63ac8a00c1d23d31b8c20526fb3fdc477
Yannic <me@yannic.at>
2025-03-22 17:54:30 +0100

    fix

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 				{
 					browser: 'chromium',
 					launch: {
-						args: ['--enable-unsafe-webgpu'],
+						args: ['--enable-unsafe-webgpu'], //'--window-position=-10000,-10000'],
 					},
 				},
 			],
