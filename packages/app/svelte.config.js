b0c4c039dd78511f1e1685982ee6494750690e00
Yannic <me@yannic.at>
2025-04-26 09:37:19 +0200

    updated sveltekit adapter

--- packages/app/svelte.config.js
+++ packages/app/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			config: './wrangler.json',
+			config: './wrangler.jsonc',
 		}),
 	},
 };
