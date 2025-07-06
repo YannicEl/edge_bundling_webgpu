fe75be6ccf71546108ec9c4bf7f466a26399124f
Yannic <me@yannic.at>
2025-07-06 16:56:13 +0200

    fix

--- packages/app/src/routes/(static)/+page.svelte
+++ packages/app/src/routes/(static)/+page.svelte
@@ -6,5 +6,4 @@
 <nav>
 	<a href="/app" class="hover:underline">Start</a>
 	<a href="/app/benchmark" class="hover:underline">Benchmark</a>
-	<a href="/app/floyd-warshall" class="hover:underline">Floyd Warshall</a>
 </nav>
