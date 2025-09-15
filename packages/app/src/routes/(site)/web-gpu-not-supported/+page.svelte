80690ef6109c562b896652f9e4e59c56a309afb8
Yannic <me@yannic.at>
2025-09-15 22:23:23 +0200

    stuff

--- packages/app/src/routes/(site)/web-gpu-not-supported/+page.svelte
+++ packages/app/src/routes/(site)/web-gpu-not-supported/+page.svelte
@@ -1,2 +1,9 @@
 <h1>WebGPU not supported.</h1>
 <p>It Looks like WebGPU is not supported on in your browser.</p>
+<p>
+	Find support for WebGPU <a
+		href="https://caniuse.com/webgpu"
+		target="_blank"
+		class="text-blue-500 underline">here</a
+	>.
+</p>
